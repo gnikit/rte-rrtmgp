9c2bdab04338f064cafd9de2c016d9366916a9ba
gnikit <giannis.nikiteas@gmail.com>
2024-05-31 19:04:48 +0100

    build(test): rename .exe to only the basename

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -8,17 +8,16 @@ else
     # Linux/Unix commands
     echo "Running on Linux/Unix"
 fi
-echo "@@@@@::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::@"
-pwd
-echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-ls -1F .
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-./rte_optic_prop_unit_tests${EXT}
-./rte_lw_solver_unit_tests${EXT}
-./rte_sw_solver_unit_tests${EXT}
-./check_equivalence${EXT} test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g256.nc 
-./check_equivalence${EXT} test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g128.nc
-./check_equivalence${EXT} test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g224.nc
-./check_equivalence${EXT} test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g112.nc
-./check_variants${EXT}    test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g256.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g128.nc
-./check_variants${EXT}    test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g224.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g112.nc 
+# Rename files ending with .exe to the same name without the extension
+for file in *.exe; do
+    mv "$file" "${file%.exe}"
+done
+./rte_optic_prop_unit_tests
+./rte_lw_solver_unit_tests
+./rte_sw_solver_unit_tests
+./check_equivalence test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g256.nc 
+./check_equivalence test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g128.nc
+./check_equivalence test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g224.nc
+./check_equivalence test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g112.nc
+./check_variants    test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g256.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g128.nc
+./check_variants    test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g224.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g112.nc 
