de2142daba0e62506e30f08be5605839c2d850a3
gnikit <giannis.nikiteas@gmail.com>
2024-05-31 18:42:34 +0100

    build(test): append .exe extension in case of Windows

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -1,10 +1,19 @@
 set -eux
-./rte_optic_prop_unit_tests
-./rte_lw_solver_unit_tests
-./rte_sw_solver_unit_tests
-./check_equivalence test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g256.nc 
-./check_equivalence test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g128.nc
-./check_equivalence test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g224.nc
-./check_equivalence test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g112.nc
-./check_variants    test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g256.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g128.nc
-./check_variants    test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g224.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g112.nc 
+EXT=""
+if [[ $(uname) == *"NT"* ]]; then
+    # Windows-specific commands
+    EXT=".exe"
+    echo "Running on Windows"
+else
+    # Linux/Unix commands
+    echo "Running on Linux/Unix"
+fi
+./rte_optic_prop_unit_tests${EXT}
+./rte_lw_solver_unit_tests${EXT}
+./rte_sw_solver_unit_tests${EXT}
+./check_equivalence${EXT} test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g256.nc 
+./check_equivalence${EXT} test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g128.nc
+./check_equivalence${EXT} test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g224.nc
+./check_equivalence${EXT} test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g112.nc
+./check_variants${EXT}    test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g256.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g128.nc
+./check_variants${EXT}    test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g224.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g112.nc 
